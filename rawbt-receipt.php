f9d08a6c7229a0b7d0b4596ff80ead7475ad4928
izmeera2000 <izmeera2000@gmail.com>
2022-12-18 21:54:21 +0800

    receipt login register

--- rawbt-receipt.php
+++ rawbt-receipt.php
@@ -1,6 +1,9 @@
 <?php
-require __DIR__ . '\assets\vendor\autoload.php';
+session_start();
 
+require __DIR__ . '\assets\vendor\autoload.php';
+require_once("assets/controller/dbcontroller.php");
+$db_handle = new DBController();
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\PrintConnectors\RawbtPrintConnector;
@@ -8,6 +11,13 @@ use Mike42\Escpos\CapabilityProfile;
 
 
 if (!empty($_GET["ref"])) {
+    $ref = $_GET["ref"];
+    $result = $db_handle->runQuery("SELECT * FROM pesanan WHERE ref='$ref'");
+
+    $date = $result[0]["timedate"];
+    $tprice = $result[0]["tprice"];
+
+    $cashier = $_SESSION["username"];
 
 }
 
@@ -17,75 +27,115 @@ try {
     /* Fill in your own connector here */
     $connector = new RawbtPrintConnector();
 
+    
     /* Information for the receipt */
-    $items = array(
-        new item("Example item #1", "4.00"),
-        new item("Another thing", "3.50"),
-        new item("Something else", "1.00"),
-        new item("A final item", "4.45"),
-    );
-    $subtotal = new item('Subtotal', '12.95');
-    $tax = new item('A local tax', '1.30');
-    $total = new item('Total', '14.25', true);
+    // $items = array(
+    //     new item("Example itasdasdasdasdasdasem #1", "4.00"),
+    //     new item("Another thing", "3.50"),
+    //     new item("Something else", "1.00"),
+    //     new item("A final item", "4.45"),
+    // );
+    // $subtotal = new item('Subtotal', '12.95');
+    $header = new item('Qty','Name', 'TPrice');
+    $header2 = new item('','', '(RM)');
+
+    $test = new item('1','item la gila', '1.30');
     /* Date is kept the same for testing */
 // $date = date('l jS \of F Y h:i:s A');
-    $date = $_GET["ref"];
 
     /* Start the printer */
-    $logo = EscposImage::load("assets\img\about-2.jpg", false);
+    $logo = EscposImage::load("assets\img\awbtlogo.png", false);
     $printer = new Printer($connector, $profile);
 
 
     /* Print top logo */
     if ($profile->getSupportsGraphics()) {
-
+        $printer->graphics($logo);
     }
     if ($profile->getSupportsBitImageRaster() && !$profile->getSupportsGraphics()) {
+        $printer->bitImage($logo);
 
     }
 
     /* Name of shop */
     $printer->setJustification(Printer::JUSTIFY_CENTER);
     $printer->selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-    $printer->text("ExampleMart Ltd.\n");
+    $printer->text("TAIPING YONG TAU FOO\n");
     $printer->selectPrintMode();
-    $printer->text("Shop No. 42.\n");
+    $printer->text("4,5,6 PUSAT PENJAJA\nBELAKANG BOMBA TAIPING\n34700 TAIPING, PERAK");
     $printer->feed();
 
 
     /* Title of receipt */
     $printer->setEmphasis(true);
-    $printer->text("SALES INVOICE\n");
+    $printer->text("CASH SALE\n\n");
     $printer->setEmphasis(false);
 
     /* Items */
     $printer->setJustification(Printer::JUSTIFY_LEFT);
     $printer->setEmphasis(true);
-    $printer->text(new item('', '$'));
+    $printer->text("Bill No:" . $ref . "\n");
+    $printer->text("Date:" . $date . "\n");
+    $printer->text("Cashier:" . $cashier . "\n\n");
+
     $printer->setEmphasis(false);
-    foreach ($items as $item) {
-        $printer->text($item->getAsString(32)); // for 58mm Font A
-    }
+    
+    // foreach ($items as $item) {
+
+    //     $printer->text($item->getAsString(32)); // for 58mm Font A
+    // }
+    
     $printer->setEmphasis(true);
-    $printer->text($subtotal->getAsString(32));
+    $printer->text($header->getAsString(32));
+
+    $printer->text($header2->getAsString(32));
+
     $printer->setEmphasis(false);
+
+    $printer->textRaw(str_repeat(chr(196), 32).PHP_EOL);
+
+    $printer->setJustification(Printer::JUSTIFY_RIGHT);
+
+
+    foreach ($result as $order) {
+
+        $namelist = json_decode($order["name"]);
+        $quantitylist = json_decode($order["quantity"]);
+        $pricelist = json_decode($order["price"]);
+        $quantityrow = 0;
+
+        foreach ($namelist as $name) {
+
+
+            $printer->text(new item($quantitylist[$quantityrow],$name, number_format($quantitylist[$quantityrow] * $pricelist[$quantityrow],2)));
+
+            ++$quantityrow;
+
+        }
+        $printer->feed(2);
+        $total = new item2('Total', number_Format($order['tprice'],2), true);
+
+     $printer->text($total->getAsString(32));
+
+    }
+
     $printer->feed();
+    $printer->selectPrintMode();
 
     /* Tax and total */
-    $printer->text($tax->getAsString(32));
+    // $printer->text($tax->getAsString(32));
     $printer->selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-    $printer->text($total->getAsString(32));
+    // $printer->text($total->getAsString(32));
     $printer->selectPrintMode();
 
     /* Footer */
     $printer->feed(2);
     $printer->setJustification(Printer::JUSTIFY_CENTER);
-    $printer->text("Thank you for shopping\n");
-    $printer->text("at ExampleMart\n");
-    $printer->text("For trading hours,\n");
-    $printer->text("please visit example.com\n");
+    $printer->text("Thank you for dining\n");
+    $printer->text("at Taiping Yong Tau Foo\n");
+
     $printer->feed(2);
-    $printer->text($date . "\n");
+    // $printer->text($date . "\n");
 
 
 
@@ -107,6 +157,48 @@ class item
 {
     private $name;
     private $price;
+
+    private $qty;
+
+    private $dollarSign;
+
+    public function __construct($qty = '',$name = '', $price = '', $dollarSign = false)
+    {
+        $this->name = $name;
+        $this->qty = $qty;
+        $this->price = $price;
+        $this->dollarSign = $dollarSign;
+    }
+
+    public function getAsString($width = 48)
+    {
+        $rightCols = 8;
+        $leftCols = 18;
+        $leftColsa = 5;
+
+        $lefta = str_pad($this->qty, $leftColsa);
+
+        $left = str_pad($this->name, $leftCols);
+
+        $right = str_pad($this->price, $rightCols, ' ', STR_PAD_LEFT);
+        return "$lefta$left$right\n";
+    }
+
+
+
+    public function __toString()
+    {
+        return $this->getAsString();
+    }
+
+}
+class item2
+{
+    private $name;
+    private $price;
+
+    private $qty;
+
     private $dollarSign;
 
     public function __construct($name = '', $price = '', $dollarSign = false)
@@ -125,10 +217,12 @@ class item
         }
         $left = str_pad($this->name, $leftCols);
 
-        $sign = ($this->dollarSign ? '$ ' : '');
+        $sign = ($this->dollarSign ? 'RM ' : '');
         $right = str_pad($sign . $this->price, $rightCols, ' ', STR_PAD_LEFT);
         return "$left$right\n";
     }
+
+
 
     public function __toString()
     {
